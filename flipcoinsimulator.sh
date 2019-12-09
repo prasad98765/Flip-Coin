708818bd2bc011b99e6c623bc949a99e90f9b25d
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-09 15:51:01 +0530

    Addad : Display Head or Tail by Random

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -2,3 +2,15 @@
 
 
 echo "Welcome : to Filp Coin Simulator"
+
+HEAD=0
+TAIL=1
+
+result=$(( RANDOM%2 ))
+
+case $result in
+   $HEAD)
+         echo "Head";;
+   $TAIL)
+         echo "Tail";;
+esac
