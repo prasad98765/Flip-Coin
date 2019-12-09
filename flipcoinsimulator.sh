8a0d137cfa7815d4bd1315476dc89454b0c78443
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-09 16:36:02 +0530

    Addad : Calculate Average of Tail or Head

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -5,12 +5,28 @@ echo "Welcome : to Filp Coin Simulator"
 
 HEAD=0
 TAIL=1
+COUNT_HEAD=0
+COUNT_TAIL=0
+declare -A dict
+for (( i=0; i<=10; i++ ))
+do
+	result=$(( RANDOM%2 ))
+		case $result in
+   		$HEAD)
+					echo "Head"
+         		dict[flip$i]='H'
+					COUNT_HEAD=$(( $COUNT_HEAD + 1 ));;
+   		$TAIL)
+         		echo "Tail"
+					dict[Flip$i]='T'
+					COUNT_TAIL=$(( $COUNT_TAIL + 1 ));;
+		esac
+done
 
-result=$(( RANDOM%2 ))
+perHead=$(( $COUNT_HEAD * 10 ))
+echo "percentage OF head" $perHead
+perTail=$(( $COUNT_TAIL * 10 ))
+echo "percentage OF head" $perTail
 
-case $result in
-   $HEAD)
-         echo "Head";;
-   $TAIL)
-         echo "Tail";;
-esac
+
+
