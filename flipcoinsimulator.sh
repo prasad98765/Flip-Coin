90d265cf374f4d3f4eeba759606c0d2012148de4
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-11 10:57:04 +0530

    Modified : Calculate Double Combination & store in dictionary

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,32 +1,51 @@
 #!/bin/bash -x
 
+number=0
 
-echo "Welcome : to Filp Coin Simulator"
+function flipcoin(){
 
-HEAD=0
-TAIL=1
-COUNT_HEAD=0
-COUNT_TAIL=0
-declare -A dict
-for (( i=0; i<=10; i++ ))
+num=$1
+number=$2
+declare -A coinset
+	for (( i=0; i<$number; i++ ))
+	do
+			coin=""
+         for (( j=0; j<$num; j++ ))
+         do
+             flipCoin=$((RANDOM%2))
+             if [ $flipCoin -eq 1 ]
+             then
+                    coin="$coin""H"
+             else
+                    coin="$coin""T"
+             fi
+          done
+             coinset[$coin]=$(( ${coinset["$coin"]} + 1 ))
+	done
+
+for val in ${!coinset[@]}
 do
-	result=$(( RANDOM%2 ))
-		case $result in
-   		$HEAD)
-					echo "Head"
-         		dict[flip$i]='H'
-					COUNT_HEAD=$(( $COUNT_HEAD + 1 ));;
-   		$TAIL)
-         		echo "Tail"
-					dict[Flip$i]='T'
-					COUNT_TAIL=$(( $COUNT_TAIL + 1 ));;
-		esac
-done
+	 perCet=$(( $((${coinset[$val]} * 100)) /20 ))
+	 echo "$val ${coinset[$val]} $perCet"
 
-perHead=$(( $COUNT_HEAD * 10 ))
-echo "percentage OF head" $perHead
-perTail=$(( $COUNT_TAIL * 10 ))
-echo "percentage OF head" $perTail
+done | sort -k3 -nr | awk 'NR==1{print ($1 " " $2 " " $3 )}'
+}
 
+main(){
 
+read -p "Enter your choice: 1.SingletCoin 2.DoubletCoin 3.TripletCoin " coin
+read -p "Enter How Many Times You Want to Flip " number
 
+        case $coin in
+        1)
+                flipcoin 1 $number;;
+        2)
+                flipcoin 2 $number;;
+        3)
+                flipcoin 3 $number;;
+        *)
+                echo "Invalid Option";;
+        esac
+
+}
+main
